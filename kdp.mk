f916e47c692ba40bc129677ce5eedbd356020e45
KangDroid <hyunwoo286@gmail.com>
2015-07-12 13:37:19 +0100

    Initial KangDroid-Project

--- kdp.mk
+++ kdp.mk
@@ -2,13 +2,13 @@
 PRODUCT_RELEASE_NAME := slteskt
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/kdp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/slteskt/device_slteskt.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_slteskt
+PRODUCT_NAME := kdp_slteskt
 PRODUCT_DEVICE := slteskt
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-G850S
